026b5cb9d9faf491fdb6ecd041e1fc54d445d605
WearyOlly <ollycloudedleopard@proton.me>
2026-04-03 15:15:22 +1000

    added UI shader from voosh's renoDX unity addon

---
+++ src/games/ultrakill/UI_0x066C98CB.ps_4_0.hlsl
@@ -0,0 +1,39 @@
+#include "../common.hlsl"
+
+Texture2D<float4> t0 : register(t0);
+SamplerState s0_s : register(s0);
+cbuffer cb0 : register(b0){
+  float4 cb0[4];
+}
+
+void main(
+  float4 v0 : SV_POSITION0,
+  float4 v1 : COLOR0,
+  float4 v2 : TEXCOORD0,
+  float4 v3 : TEXCOORD1,
+  float4 v4 : TEXCOORD2,
+  out float4 o0 : SV_Target0)
+{
+  float4 r0,r1;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.x = 255 * v1.w;
+  r0.x = round(r0.x);
+  r0.w = (1.0 / 255.0) * r0.x;
+  r1.xyzw = t0.Sample(s0_s, v2.xy).xyzw;
+  r1.xyzw = cb0[3].xyzw + r1.xyzw;
+  r0.xyz = v1.xyz;
+  r0.xyzw = r1.xyzw * r0.xyzw;
+  o0.xyz = r0.xyz * r0.www;
+  o0.w = r0.w;
+  o0.w = saturate(o0.w);
+  if(injectedData.rolloffUI == 1.f){
+    o0.xyz = injectedData.gammaSpace != 0.f ? renodx::color::srgb::DecodeSafe(o0.xyz) : o0.xyz;
+    o0.xyz = rolloff(o0.xyz, 0.85f, true);
+    o0.xyz = injectedData.gammaSpace != 0.f ? renodx::color::srgb::EncodeSafe(o0.xyz) : o0.xyz;
+  } else if(injectedData.rolloffUI == 2.f){
+    o0.xyz = saturate(o0.xyz);
+  }
+  return;
+}
